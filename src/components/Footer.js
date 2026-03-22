1387a3ca8e09e25112d0772fafe19a1591ee3c2d
aatmikbhagat <aatmik@gmail.com>
2026-03-22 00:47:55 -0400

    Changed laurier icon to white

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
                 <a href="https://cs-webring.vercel.app/go?site=https%3A%2F%2Faatmikbhagat.com%2F&nav=prev">←</a>
                 <a href="https://cs-webring.vercel.app/">
                     <img
-                        src="https://cs-webring.vercel.app/icon.black.svg"
+                        src="https://cs-webring.vercel.app/icon.white.svg"
                         alt="CS Webring"
                         style={{ width: "24px", height: "auto", opacity: 0.8 }}
                     />
